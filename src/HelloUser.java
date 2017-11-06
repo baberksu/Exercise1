738a3756f97fdbb8293ebc367cb56d2d3f3142cb
barisalpberksu <barisalpberksu@w200-65-v4.eduroam.dynamic.rbg.tum.de>
2017-11-06 10:08:25 +0100

    Exercise 1

---
+++ src/HelloUser.java
@@ -0,0 +1,15 @@
+
+public class HelloUser {
+	
+	private String userName;
+	
+	public HelloUser(String userName) {
+		this.userName = userName;
+	
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello "+userName+"!");
+	}
+	
+}
